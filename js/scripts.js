80ca25cd9f16dfa30b4d627a33126f9abfd8b401
alanapogostin <75394059+alanapogostin@users.noreply.github.com>
2021-03-23 23:52:28 -0400

    getting the map and chloropleth lables and sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -11,95 +11,93 @@ var map = new mapboxgl.Map({
 });
 
 var nav = new mapboxgl.NavigationControl();
-    map.addControl(nav, 'top-left');
+map.addControl(nav, 'top-left');
 
-map.on('style.load', function (){
-      //add the geo source
-      map.addSource('57th_lots' , {
-        type: 'geojson',
-        data: 'data/all_lots.geojson'
-    });
-  })
+map.on('style.load', function() {
+  //add the geo source
+  map.addSource('57th_lots', {
+    type: 'geojson',
+    data: 'data/all_lots.geojson'
+  });
+})
 
-  // Adding 3d Buildings
-  map.on('load', function () {
+// Adding 3d Buildings
+map.on('load', function() {
   // Insert the layer beneath any symbol layer.
   var layers = map.getStyle().layers;
 
   var labelLayerId;
   for (var i = 0; i < layers.length; i++) {
-  if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
-  labelLayerId = layers[i].id;
-  break;
+    if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+      labelLayerId = layers[i].id;
+      break;
+    }
   }
-  }
-  map.addLayer(
-              {
-              'id': '3d-buildings',
-              'source': 'composite',
-              'source-layer': 'building',
-              'filter': ['==', 'extrude', 'true'],
-              'type': 'fill-extrusion',
-              'minzoom': 15,
-              'paint': {
-              'fill-extrusion-color': '#aaa',
-
-              // use an 'interpolate' expression to add a smooth transition effect to the
-              // buildings as the user zooms in
-              'fill-extrusion-height': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              15,
-              0,
-              15.05,
-              ['get', 'height']
-              ],
-              'fill-extrusion-base': [
-              'interpolate',
-              ['linear'],
-              ['zoom'],
-              15,
-              0,
-              15.05,
-              ['get', 'min_height']
-              ],
-              'fill-extrusion-opacity': 0.6
-              }
-          },
-          labelLayerId
-        );
-
-/// Adding Number of Floors Fill
   map.addLayer({
-      'id': '57th_street_floors_fill',
-      'type': 'fill',
-      'source': '57th_lots',
-      'layout': {},
+      'id': '3d-buildings',
+      'source': 'composite',
+      'source-layer': 'building',
+      'filter': ['==', 'extrude', 'true'],
+      'type': 'fill-extrusion',
+      'minzoom': 15,
       'paint': {
-          'fill-color': [
-            'interpolate',
-            ['linear'],
-            ['get', 'NumFloors'],
-            0,
-            '#fef0d9',
-            20,
-            '#fdcc8a',
-            40,
-            '#fc8d59',
-            60,
-            '#e34a33',
-            80,
-            '#b30000'
-          ]
-        ,
-        'fill-outline-color': '#ccc',
-        'fill-opacity': 0.8
-      }
-    });
-  })
+        'fill-extrusion-color': '#aaa',
 
-  map.on('load', function() {
+        // use an 'interpolate' expression to add a smooth transition effect to the
+        // buildings as the user zooms in
+        'fill-extrusion-height': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          15,
+          0,
+          15.05,
+          ['get', 'height']
+        ],
+        'fill-extrusion-base': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          15,
+          0,
+          15.05,
+          ['get', 'min_height']
+        ],
+        'fill-extrusion-opacity': 0.6
+      }
+    },
+    labelLayerId
+  );
+
+  /// Adding Number of Floors Fill
+  map.addLayer({
+    'id': '57th_street_floors_fill',
+    'type': 'fill',
+    'source': '57th_lots',
+    'layout': {},
+    'paint': {
+      'fill-color': [
+        'interpolate',
+        ['linear'],
+        ['get', 'NumFloors'],
+        0,
+        '#fef0d9',
+        20,
+        '#fdcc8a',
+        40,
+        '#fc8d59',
+        60,
+        '#e34a33',
+        80,
+        '#b30000'
+      ],
+      'fill-outline-color': '#ccc',
+      'fill-opacity': 0.8
+    }
+  });
+})
+
+map.on('load', function() {
   var layers = ['0-20', '20-40', '40-60', '60-80', '80+'];
   var colors = ['#fef0d9', '#fdcc8a', '#fc8d59', '#e34a33', '#b30000'];
 
@@ -118,17 +116,17 @@ map.on('style.load', function (){
     legend.appendChild(item);
   }
 
-  });
+});
 
-  // Adding a interactive feature
+// Adding a interactive feature
 var popup = new mapboxgl.Popup({
   closeButton: false,
   closeOnClick: false
 });
-map.on('mousemove', function (e) {
+map.on('mousemove', function(e) {
 
   var features = map.queryRenderedFeatures(e.point, {
-      layers: ['57th_street_floors_fill'],
+    layers: ['57th_street_floors_fill'],
   });
 
   if (features.length > 0) {
@@ -151,10 +149,9 @@ map.on('mousemove', function (e) {
     popup.setLngLat(e.lngLat).setHTML(popupContent).addTo(map);
     //map.getSource('all_lots').setData(hoveredFeature.geometry);
     map.getCanvas().style.cursor = 'pointer';
-  }
-  else {
+  } else {
     popup.remove();
-    map.getCanvas().style.cursor = '';
-  ;}
+    map.getCanvas().style.cursor = '';;
+  }
 
 })
